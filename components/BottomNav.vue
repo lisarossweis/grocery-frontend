7ecb367e698e10c2f105d0523372615a8c76bd1c
Bukeuw <ato@ukeuw.com>
2023-08-08 10:36:44 +0700

    Refactor: move bottom nav to its own component

---
+++ components/BottomNav.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bottom-nav">
+    <router-link
+      class="tab"
+      v-for="(nav, index) in bottomNavLinks"
+      :key="index"
+      :to="nav.link"
+    >
+      <p>
+        <fa-icon :icon="nav.icon"></fa-icon>
+      </p>
+      <p class="sr-only">{{ nav.text }}</p>
+    </router-link>
+  </div>
+</template>
+
+<script setup lang="ts">
+const bottomNavLinks = useBottomNavLinks()
+</script>
