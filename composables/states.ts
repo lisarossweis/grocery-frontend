3f57392f6d4f4908566c9d88e3e132dac8e6a79d
Bukeuw <ato@ukeuw.com>
2023-08-08 08:12:29 +0700

    Refactor: move state from individual component to dedicated file to make code cleaner

---
+++ composables/states.ts
@@ -0,0 +1,33 @@
+// application state
+import Unit from '@/utils/unit'
+
+export const useCategories = () => useState<Category[]>('categories', () => [
+  { title: 'All', link: '/all', icon: '/assets/icons/groceries.svg' },
+  { title: 'Vegetables', link: '/vegetables', icon: '/assets/icons/vegetables.svg' },
+  { title: 'Meat', link: '/meat', icon: '/assets/icons/meat.svg' },
+  { title: 'Fruits', link: '/fruits', icon: '/assets/icons/fruits.svg' },
+  { title: 'Fish', link: '/fish', icon: '/assets/icons/fish.svg' },
+])
+
+export const useProducts = () => useState<Product[]>('products', () => [
+  { name: 'Apple', unit: Unit.Ounce, price: 10, category: 'fruits', image: '/assets/icons/apple.svg' },
+  { name: 'Orange', unit: Unit.Ounce, price: 10, category: 'fruits', image: '/assets/icons/orange.svg' },
+  { name: 'Grape', unit: Unit.Ounce, price: 10, category: 'fruits', image: '/assets/icons/grape.svg' },
+  { name: 'Carrot', unit: Unit.Ounce, price: 10, category: 'vegetables', image: '/assets/icons/carrot.svg' },
+  { name: 'Garlic', unit: Unit.Ounce, price: 10, category: 'vegetables', image: '/assets/icons/garlic.svg' },
+  { name: 'Potato', unit: Unit.Ounce, price: 5, category: 'vegetables', image: '/assets/icons/potato.svg' },
+  { name: 'Green Onion', unit: Unit.Ounce, price: 0, category: 'vegetables', image: '/assets/icons/green-onion.svg' },
+  { name: 'Tomato', unit: Unit.Ounce, price: 10, category: 'vegetables', image: '/assets/icons/tomato.svg' },
+  { name: 'Chicken', unit: Unit.Ounce, price: 20, category: 'meat', image: '/assets/icons/chicken.svg' },
+  { name: 'Fish', unit: Unit.Ounce, price: 15, category: 'fish', image: '/assets/icons/fish.svg' },
+])
+
+export const useCurrencySymbol = () => useState<string>('currencySymbol', () => 'Rp')
+
+export const useBottomNavLinks = () => useState('bottomNavLinks', () => [
+  { text: 'Home', link: '/', icon: '' },
+  { text: 'Wishlist', link: '/wishlist', icon: '' },
+  { text: 'Bag', link: '/bag', icon: '' },
+  { text: 'Orders', link: '/orders', icon: '' },
+  { text: 'Profile', link: '/profile', icon: '' },
+])
