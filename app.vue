610f361581b0a81376d349b1859616e1a503bf8f
Bukeuw <ato@ukeuw.com>
2023-08-08 10:36:07 +0700

    Create default layout for the apps

--- app.vue
+++ app.vue
@@ -1,5 +1,3 @@
 <template>
-  <div>
-    <router-view />
-  </div>
+  <NuxtLayout></NuxtLayout>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <router-view></router-view>
+
+    <div class="spacer"></div>
+    <BottomNav></BottomNav>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { BottomNav } from '#components'
+</script>
