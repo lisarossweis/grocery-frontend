756b6b5546fbc9070e8d22bc254ddf4bee3609f9
Bukeuw <ato@ukeuw.com>
2023-08-08 10:37:12 +0700

    Refactor: move topbar to its own component

---
+++ components/TopBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="topbar">
+    <div class="logo">GiaGrocery</div>
+    <div class="searchbox">
+      <fa-icon icon="fa-solid fa-search"></fa-icon>
+      <input class="input" type="search" v-show="showSearchBox">
+      <button class="btn btn-primary" v-show="showSearchBox" @click="hideSearchBox">
+        Cancel
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+let showLogo = true
+let showSearchBox = false
+const expandSearchBox = () => {
+  showSearchBox = true
+  showLogo = false
+}
+const hideSearchBox = () => {
+  showSearchBox = false
+  showLogo = true
+}
+</script>
--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,13 +1,15 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faUser, faHome, faHeart, faReceipt, faBagShopping } from '@fortawesome/free-solid-svg-icons'
+import { faUser, faHome, faHeart, faReceipt, faBagShopping, faSearch } from '@fortawesome/free-solid-svg-icons'
 
 export default defineNuxtPlugin((nuxtApp) => {
   library.add(
     faHome,
     faHeart,
     faBagShopping,
-    faReceipt,faUser
+    faReceipt,
+    faUser,
+    faSearch
   )
   nuxtApp.vueApp.component('fa-icon', FontAwesomeIcon)
 })
