0553fb9f5f207117c2db39d47d1eedd4595ee9da
Bukeuw <ato@ukeuw.com>
2023-08-08 08:11:25 +0700

    Add helper type definition and enum

---
+++ utils/entities.d.ts
@@ -0,0 +1,13 @@
+type Category = {
+  title: string
+  link: string
+  icon: string
+}
+
+type Product = {
+  name: string
+  price: number
+  image: string
+  category: string
+  unit: Unit
+}
---
+++ utils/unit.ts
@@ -0,0 +1,8 @@
+enum Unit {
+  Gram = 'g',
+  Ounce = 'oz',
+  Kilogram = 'kg',
+  Piece = 'piece',
+}
+
+export default Unit
