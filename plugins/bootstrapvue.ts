384dd9763c9a7e225554472de85a0bb13bbfb1e1
Bukeuw <ato@ukeuw.com>
2023-08-08 22:20:11 +0700

    Add plugin for bootstrap, just the css without the components bootstrap-vue doesn't play nice with nuxt 3

---
+++ plugins/bootstrapvue.ts
@@ -0,0 +1,10 @@
+//import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap-vue/dist/bootstrap-vue-icons.min.css'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  //nuxtApp.vueApp.use(BootstrapVue)
+  //nuxtApp.vueApp.use(BootstrapVueIcons)
+})
